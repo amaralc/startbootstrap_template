72dbe8929661b12f1780d3f549b08ee7e42896c0
amaralc <amaral.calil@gmail.com>
2020-05-24 17:32:27 -0300

    FEATURE / CONTENT / Restructured navbar as component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import Navbar from './components/Navbar';
+
+export default function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            Start Bootstrap
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home
-                  <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
       <header className="bg-primary py-5 mb-5">
         <div className="container h-100">
@@ -186,5 +146,3 @@ function App() {
     </>
   );
 }
-
-export default App;
