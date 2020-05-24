bd2be47c3c4d87974fcc421fc9c9f6648433be5b
amaralc <amaral.calil@gmail.com>
2020-05-24 18:50:38 -0300

    FEATURE / CONTENT / Add Link library and return to Start Bootstrap description

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
@@ -6,7 +7,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
           <a className="navbar-brand" href="/">
-            peerLab
+            Start Bootstrap
           </a>
           <button
             className="navbar-toggler"
@@ -21,26 +22,26 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
                   Home
-                  <span className="sr-only">(current)</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/">
                   About
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/">
                   Services
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/">
                   Contact
-                </a>
+                  <span className="sr-only">(current)</span>
+                </Link>
               </li>
             </ul>
           </div>
